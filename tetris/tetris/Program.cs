35f23ed2362e6d3959de9c6d4ba28aa52fcfc6ee
sana5695 <deep.beat@yandex.ru>
2020-11-29 22:34:07 +0500

    вывели две точки на экран

--- tetris/tetris/Program.cs
+++ tetris/tetris/Program.cs
@@ -6,7 +6,21 @@ namespace tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+            int x1 = 2;
+            int y1 = 2;
+            int x2 = 3;
+            int y2 = 2;
+            char c1 = '*';
+            Drow(x1, y1, c1);
+            Drow(x2, y2, c1);
+            Console.ReadLine();
+        }
+        static void Drow(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
